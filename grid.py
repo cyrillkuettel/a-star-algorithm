4c391939654a0c29a68204b7f528ecb78159f854
cyrillkuettel <cyrillkuettel@gmail.com>
2021-08-20 18:17:36 +0200

    squares updated: if square clicked -> not_traversable List updated

--- grid.py
+++ grid.py
@@ -3,23 +3,48 @@ import pygame
 pygame.init()
 pygame.display.set_caption('A* Algorithm')
 blocksize = 50
-gridDisplay = pygame.display.set_mode((200, 200))
+NUMBER_OF_BOXES = 25  # boxes per line
+LENGTH = NUMBER_OF_BOXES * blocksize
+gridDisplay = pygame.display.set_mode((LENGTH, LENGTH))
 pygame.display.get_surface().fill((200, 100, 200))  # background
+
+# Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-matrix = [[1, 1, 0, 1],
-          [1, 0, 0, 1],
-          [1, 1, 0, 1],
-          [1, 1, 1, 1]]
-
 not_traversable = []
-
+"""
 # To declare a multidimensional list of zeros in python you have to use a list comprehension like this:
-matrix2 = [[0 for col in range(4)] for row in range(4)]
-
-
+matrix2 = [[0 for col in range(NUMBER_OF_BOXES)] for row in range(NUMBER_OF_BOXES)]
 # to avoid reference sharing between the rows.
+"""
+
+matrix2 = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [-1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [-1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [-1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+matrix2 = list(map(list, zip(*matrix2)))
 
 
 def createSquare(x, y, color):
@@ -40,7 +65,25 @@ def initializeGrid():
     pygame.display.update()
 
 
-def set_Cell_untravsersable(mouse_click_event):
+def compute_Rectangle_From_MousePosition(event):
+    # determine mouse position
+    _mpos_x, _mpos_y = event.pos
+    # determine cell number
+    coordinate = _mpos_x // blocksize, _mpos_y // blocksize
+    rect = pygame.Rect(coordinate[0] * blocksize, coordinate[1] * blocksize,
+                       blocksize, blocksize)
+    return rect, coordinate
+
+
+def set_start_Point(event):
+    rect, coordinate = compute_Rectangle_From_MousePosition(event)
+    pygame.draw.rect(gridDisplay, BLACK, rect)
+    # This is kind of not very pretty.. I use two data structures, creating redundancy.
+    not_traversable.append(coordinate)
+    matrix2[coordinate[0]][coordinate[1]] = -1
+
+
+def set_Cell_untraversable(mouse_click_event):
     # determine mouse position
     _mpos_x, _mpos_y = mouse_click_event.pos
     # determine cell number
@@ -48,16 +91,24 @@ def set_Cell_untravsersable(mouse_click_event):
     rect = pygame.Rect(coordinate[0] * blocksize, coordinate[1] * blocksize,
                        blocksize, blocksize)
     pygame.draw.rect(gridDisplay, BLACK, rect)
+    # This is kind of not very pretty.. I use two data structures, creating redundancy
     not_traversable.append(coordinate)
+    matrix2[coordinate[0]][coordinate[1]] = -1
 
 
-initializeGrid()
+if __name__ == "__main__":
+    initializeGrid()
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            set_Cell_untravsersable(event)
-        pygame.display.update()
+    running = True
+    count_right_clicks = 0
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # left click
+                    set_Cell_untraversable(event)
+                if event.button == 3:  # right click
+                    count_right_clicks += 1
+            pygame.display.update()
